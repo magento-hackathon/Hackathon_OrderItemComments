f380aa8fb117dff65be34a5b9c7ad30e7c29606c
peterjaap <peterjaap@elgentos.nl>
2016-01-23 15:36:07 +0100

    Added controller (Branch: master)

---
+++ src/Hackathon/OrderItemComments/Controller/Comment/Save.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hackathon\OrderItemComments\Controller\Comment;
+
+class Save extends \Magento\Framework\App\Action\Action
+{
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        die('Works! Time for beer');
+    }
+}
