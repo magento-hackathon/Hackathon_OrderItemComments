a69c3f4ade60f5c4dea78533eb59dc819c45beff
Wouter Steenmeijer <wouter@elgentos.nl>
2016-01-23 16:36:09 +0100

    Added block, layout xml file and template

---
+++ Block/Ordercomments.php
@@ -0,0 +1,14 @@
+<?php
+namespace Hackathon\OrderItemComments\Block;
+
+/**
+ * Baz block
+ */
+class Ordercomments
+    extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "Foo Bar Baz";
+    }
+}
